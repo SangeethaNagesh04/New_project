28189866b2160647faa6e034f358662f43a87d82
LENOVO <LENOVO@sangeetha>
2021-11-21 06:45:00 +0530

    commited file

--- new_1/src/new_1/class_1.java
+++ new_1/src/new_1/class_1.java
@@ -4,7 +4,8 @@ public class class_1 {
 	public static void main(String args[]) {
 		System.out.println("hi");
 		System.out.println("hi nagesh");
-		
+		System.out.println("hi sampi");
+		System.out.println("hi sangeetha");
 	}
 
 }
